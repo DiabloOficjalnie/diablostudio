939f69a9e2aa0461c38ecb659fe15539187f4f84
Mateusz Mejza <mateuszmejza@Mateuszs-MacBook-Pro.local>
2025-10-08 12:43:23 +0200

    Clerk

--- src/app/client/dashboard/page.tsx
+++ src/app/client/dashboard/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useUser } from '@clerk/nextjs'
 import { useRouter } from 'next/navigation'
 import { currentUser } from '@clerk/nextjs/server'
